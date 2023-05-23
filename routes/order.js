2810c9939163e3987d6d12fd44908bc6ac0afa0a
Jay <rright815@gmail.com>
2023-05-23 21:23:13 +1000

    CRUDwithJWT

--- routes/order.js
+++ routes/order.js
@@ -1,4 +1,66 @@
+const Order = require("../models/Order");
+const Product = require("../models/Product");
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
+
 const router = require("express").Router();
 
+//CREATE
 
-module.exports =router;
+router.post("/", verifyToken, async (req,res)=>{
+        const neworder = new Order(req.body)
+
+        try{
+            const savedorder = await neworder.save();
+            res.status(200).json(savedorder);
+        }catch(err){
+            res.status(500).json(err)
+        }
+})
+
+//UPDATE
+router.put("/:id", verifyTokenAndAdmin, async (req,res)=>{
+
+   try{
+    const updatedOrder = await Order.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+    },{new:true}
+    );
+    res.status(200).json(updatedOrder);
+   }catch(err){res.status(500).json(err);
+}
+});
+
+//DELETE
+
+router.delete("/:id", verifyTokenAndAdmin, async(req,res)=>{
+    try{
+        await Order.findByIdAndDelete(req.params.id);
+        res.status(200).json("Order has been deleted...");
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+//GET USER ORDERS
+
+router.get("/find/:userId", verifyTokenAndAuthorization, async(req,res)=>{
+    try{
+        const orders = await Order.find({userId: req.params.userId});
+        res.status(200).json(orders);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+// //GET ALL 
+
+router.get("/", verifyTokenAndAdmin, async (req,res)=>{
+    try{
+        const orders = await Order.find();
+        res.status(200).json(orders);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+
+ module.exports =router;
